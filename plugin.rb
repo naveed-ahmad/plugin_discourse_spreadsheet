2e2e8610ca8314e4265fe8c6d5b0aa6903356a88
Daniel Fernández <dfernandez@vairix.com>
2014-09-25 12:13:20 -0300

    remove header from spreadsheet

--- plugin.rb
+++ plugin.rb
@@ -9,6 +9,6 @@ class Onebox::Engine::SpreadsheetOnebox
   matches_regexp(/^https?:\/\/(?:www\.)?(?:m\.)?(?:docs\.google\.com\/spreadsheet)\/.+$/)
 
   def to_html
-    "<iframe src='#{@url}' style='border: 0' width='800' height='600' frameborder='0' scrolling='no'></iframe>"
+    "<iframe src='#{@url}&rm=minimal' style='border: 0' width='800' height='600' frameborder='0' scrolling='no'></iframe>"
   end
 end
